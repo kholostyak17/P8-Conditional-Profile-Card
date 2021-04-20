cbdc9a199bbdb953bb3f0e767cb551bab94dad2b
kholostyak17 <kholostyak17@gmail.com>
2021-04-20 17:01:24 +0000

    added new default avatar

--- src/js/index.js
+++ src/js/index.js
@@ -56,14 +56,15 @@ window.onload = function() {
     // this is the url of the image that will used as background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL:
+      "https://cdn.iconscout.com/icon/free/png-256/avatar-373-456325.png",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: "user",
-    github: "user",
-    linkedin: "user",
-    instagram: "user",
+    twitter: "",
+    github: "",
+    linkedin: "",
+    instagram: "",
     name: "Nombre",
     lastname: "Apellido",
     role: "Cargo",
